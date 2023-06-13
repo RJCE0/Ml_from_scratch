20466662b75407c63e1836dec9e56ad926190f55
Reece Jackson <Rjackson2000@icloud.com>
2023-06-13 15:18:35 +0100

    chore(pca): Added Docstring for pca.py

--- PCA_Model/pca.py
+++ PCA_Model/pca.py
@@ -1,5 +1,20 @@
 import numpy as np
+"""
+    Concept of PCA:
+    PCA can be seen as a subset of the concept of SVD, which is the generalisation of eigen decomposition. 
+    
+    Process: 
+    1. Compute mean for every dimension of the dataset
+    2. Compute Covariance matrix for whole dataset
+    3. Compute eigenvectors and corresponding eigenvalues
+    4. Sort the eigenvectors by decreasing eigenvalues and choose first k vectors depending on 
+    how many dimensions you wish to reduce to. (Roughly speaking, the eigenvectors with 
+    lowest eigenvalues bear the least information about the distribution of the data)
+    5. Transform all the points by this new matrix formed of the first k vectors onto the new subspace
 
+    Returns:
+        The set of points X mapped onto the new subspace
+"""
 
 class PCA:
     def __init__(self, n_pcs):
